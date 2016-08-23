51ce154384a611575d2f91b9929450fd0f8fa376
Robin Deits <robin.deits@gmail.com>
2016-08-23 17:49:01 -0400

    copy in custom info.plist

--- osx/package_app.py
+++ osx/package_app.py
@@ -78,6 +78,7 @@ def bootstrap(source_app, appname, confirm):
     print('copying {} to {}'.format(source_app, appname))
     sh.mkdir(appname)
     sh.cp('-a', join(source_app, "Contents"), join(appname, "Contents"))
+    sh.cp("../../../../../Info.plist", join(appname, "Contents"))
 
 
 def insert_app(path_to_app, appname, blacklist=None, whitelist=None):
